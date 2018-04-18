1aa9997ccad063835abac3f91559b6025eef0333
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-04-18 17:52:01 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -12,5 +12,11 @@ end
 
 def word_substituter(tweet)
   #takes in string of tweet as an argument and shortens it based on substitutes
-  
+  short_tweet = [ ]
+  arr_dictionary = dictionary.keys
+  arr_tweets = str_tweets.split(' ')
+  arr_tweets.each do |word|
+    arr_dictionary.include?(word) ? short_tweet << dictionary[word] : result << word
+  end
+  result.join(' ')
 end
