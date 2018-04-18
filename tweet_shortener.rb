9baf3e33433055a88bb43584b1114d253946afe6
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-04-18 18:53:48 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,14 +1,18 @@
 # Write your code here.
-def dictionary
-  dictionary = {
-  "hello" => 'hi'
-  "to, two, too" => '2' 
-  "for, four" => '4'
-  'be' => 'b'
-  'you' => 'u'
-  "at" => "@" 
-  "and" => "&"
+def dictionary 
+  {
+"hello" => 'hi',
+"to" => '2',
+"two" => '2',
+"too" => '2', 
+"for" => '4',
+"four" => '4',
+'be' => 'b',
+'you' => 'u',
+"at" => "@",
+"and" => "&"
 }
+
 end
 
 # def word_substituter(tweet)
