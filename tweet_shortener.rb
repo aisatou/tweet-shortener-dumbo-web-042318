ef5561c7ac216db52697e810ce81379828883100
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-04-18 19:00:04 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -15,28 +15,4 @@ end
 
 def word_substituter(tweet)
   #takes in string of tweet as an argument and shortens it based on substitutes
-  short = tweet.split(" ")
-  short_tweet = []
-
-  tweet.each do |word|
-    case word.downcase
-    if "hello"
-      short_tweet << "hi"
-    elsif "to", "two", "too"
-      short_tweet << "2"
-    elsif "for", "four" 
-      short_tweet << "4"
-    elsif "be" 
-      short_tweet << "b"
-    elsif "you" 
-      short_tweet << "u"
-    elsif "at" 
-      short_tweet << "@" 
-    elsif "and" 
-      short_tweet << "&"
-    else
-      short_tweet << word
-    end
-  end
-  short_tweet.join(" ")
-  end
+  
