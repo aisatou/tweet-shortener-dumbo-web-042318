467a716819ba2efcf857511e6db38096e3880a72
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-04-18 19:23:09 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -49,5 +49,5 @@ def shortened_tweet_truncator(tweet)
   #truncates tweets over 140 characters after shortening
   #truncate with ellipsis(...)
   if tweet.size > 140
-    tweet
+    tweet[0...140]
 end
